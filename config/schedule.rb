72b03e9111eca7b278457694452aee78214f7f00
tim <tim.w@axsoftware.net>
2017-06-27 16:33:15 +0800

    make frequency configuable

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,12 @@
-set :output, '/dev/null'
+require 'yaml'
 
-every 1.hours do 
+set :output, '/dev/null'
+freq = YAML.load_file(File.dirname(__FILE__)+'/kabe_setting.yaml')['freq']
+
+every freq['kabe'].minutes do 
 	command "cd #{path};ruby ./lib/kabe.rb"
 end
 
-every 15.minutes do 
+every freq['chwall'].minutes do 
 	command "cd #{path};sh ./lib/chwall.sh"
 end
--- config/windows_schedule.rb
+++ config/windows_schedule.rb
@@ -1,5 +1,7 @@
+require 'yaml'
 PATH = Dir.pwd 
 HIDE_WIN = PATH + '/lib/hidewin.vbs'
+FREQ = YAML.load_file(File.dirname(__FILE__)+'/kabe_setting.yaml')['freq']
 
 def remove_old_sche
 	`schtasks /delete /TN haijin_kabe_get /f >NUL 2>&1`
@@ -8,9 +10,9 @@ end
 
 def new_sche
 	# rescraping images hourly
-	`schtasks /create /TN haijin_kabe_get /SC HOURLY /MO 1 /TR "#{HIDE_WIN} ruby #{PATH}/lib/kabe.rb " `
-	#change wallpaper 15 mins
-	`schtasks /create /TN haijin_kabe_change /SC MINUTE /MO 15 /TR "#{HIDE_WIN} ruby #{PATH}/lib/chwall_win.rb" `
+	`schtasks /create /TN haijin_kabe_get /SC MINUTE /MO #{FREQ['kabe']} /TR "#{HIDE_WIN} ruby #{PATH}/lib/kabe.rb " `
+	# change wallpaper 15 mins
+	`schtasks /create /TN haijin_kabe_change /SC MINUTE /MO #{FREQ['chwall']} /TR "#{HIDE_WIN} ruby #{PATH}/lib/chwall_win.rb" `
 end
 
 remove_old_sche
