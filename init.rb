59e3c2330d571f42344db1e723eb2f1befcd5908
tim <tim.w@axsoftware.net>
2017-09-15 16:32:25 +0800

    add output, delete comments

--- init.rb
+++ init.rb
@@ -1,15 +1,5 @@
 puts 'Initializing...'
-# require_relative 'lib/download'
+puts 'Downloading images'
+require_relative 'lib/download'
+puts 'Setting crontab'
 require_relative 'cron/init_cronjob'
-
-# if OS.windows?
-#   puts 'preparing process for windows'
-#   `ruby config/windows_schedule.rb`
-  
-# elsif OS.posix?
-#   puts 'Avoiding windows newline crush'
-#   `sed -i 's/\r$//' ./lib/chwall.sh`
-
-#   puts 'Setting crontab...'
-#   `whenever --update-crontab`
-# end
