f677f9581b0886253e65d4641fffb4031ac56555
tim <tim.w@axsoftware.net>
2017-09-16 01:18:26 +0800

    fix api pattern

--- lib/source_factory.rb
+++ lib/source_factory.rb
@@ -11,7 +11,7 @@ class SourceFactory
       raise 'Inlalide Account Info' unless opt[:pixiv]
       @product = Pixiv.new account: opt[:pixiv]['account'], passowrd: opt[:pixiv]['password']
     when 'pixiv.moe'
-      @product = ThirdPartyApi.new host_name: 'https://api.pixiv.moe/', path: 'v2/ranking', url_pattern: /large":"(https:.\/.\/api\.pixiv\.moe.\/v[0-9].\/image.\/[a-zA-Z0-9]*=)/
+      @product = ThirdPartyApi.new host_name: 'https://api.pixiv.moe/', path: 'v2/ranking', url_pattern: /original_image_url":"(https:.\/.\/api\.pixiv\.moe.\/v[0-9].\/image.\/[a-zA-Z0-9]*=?)/
     when 'imjad.cn'
       @product = ThirdPartyApi.new referer: 'https://pixiv.net/', host_name: 'https://api.imjad.cn/', path: 'pixiv/v1/?type=rank&mode=daily&content=illust', url_pattern: /large":"(https:.\/.\/i2\.pixiv\.net.\/img-original.\/img.\/([0-9]*.\/){6}[0-9]*(_p[0-9])?\.(jpg|png))/
     end
